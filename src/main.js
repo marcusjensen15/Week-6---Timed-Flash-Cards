2af338db05f9efccca93908b94434c9b61444db7
Student <example@example.com>
2019-11-18 16:24:38 -0800

    Backend testing passed. populating deck of flashcards. now testing backend with timeout function

--- src/main.js
+++ src/main.js
@@ -1,11 +1,33 @@
-// import { test } from  './dice-game.js';
+import { Cards, MyCard } from  './dice-game.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-// function test2() {
-//   console.log('this is the front end');
-// } test2();
+console.log("hi");
 
-console.log("pig");
+let myCard= new MyCard("Hello my friend", "You look nice");
+let myCard2= new MyCard("oink", "pig");
+
+console.log(myCard);
+let cards = new Cards();
+console.log(cards);
+cards.addCard(myCard.frontSide);
+cards.addCard(myCard.backSide);
+cards.addCard(myCard2.frontSide);
+cards.addCard(myCard2.backSide);
+
+console.log(cards);
+
+
+// const showCards(deck) => {
+//
+//   for(let i = 0; i < deck.length; i ++){
+//     setTimeout(function() { console.log(deck[i]) }, 2000);
+//
+//   }
+// };
+//
+// showCards(cards.completedCards);
+//
+// console.log("hi");
